2c28d50e437314800451c3aa28713f9df8afc1d7
Loïc Damien <loic.damien@dzamlo.ch>
2017-06-18 21:27:28 +0200

    rustfmt

--- tests/lib.rs
+++ tests/lib.rs
@@ -21,71 +21,71 @@ simple_bitfield! {
 
 #[test]
 fn test_single_bit() {
-   let mut fb = FooBar(0);
+    let mut fb = FooBar(0);
 
-   fb.set_foo1(1);
-   assert_eq!(0x1, fb.0);
-   assert_eq!(0x1, fb.foo1());
-   assert_eq!(0x0, fb.foo2());
+    fb.set_foo1(1);
+    assert_eq!(0x1, fb.0);
+    assert_eq!(0x1, fb.foo1());
+    assert_eq!(0x0, fb.foo2());
 
-   fb.set_foo2(1);
-   assert_eq!(0x80000001, fb.0);
-   assert_eq!(0x1, fb.foo1());
-   assert_eq!(0x1, fb.foo2());
+    fb.set_foo2(1);
+    assert_eq!(0x80000001, fb.0);
+    assert_eq!(0x1, fb.foo1());
+    assert_eq!(0x1, fb.foo2());
 
-   fb.set_foo1(0);
-   assert_eq!(0x80000000, fb.0);
-   assert_eq!(0x0, fb.foo1());
-   assert_eq!(0x1, fb.foo2());
+    fb.set_foo1(0);
+    assert_eq!(0x80000000, fb.0);
+    assert_eq!(0x0, fb.foo1());
+    assert_eq!(0x1, fb.foo2());
 }
 
 #[test]
 fn test_single_bit_plus_garbage() {
-   let mut fb = FooBar(0);
+    let mut fb = FooBar(0);
 
-   fb.set_foo1(0b10);
-   assert_eq!(0x0, fb.0);
-   assert_eq!(0x0, fb.foo1());
-   assert_eq!(0x0, fb.foo2());
+    fb.set_foo1(0b10);
+    assert_eq!(0x0, fb.0);
+    assert_eq!(0x0, fb.foo1());
+    assert_eq!(0x0, fb.foo2());
 
-   fb.set_foo1(0b11);
-   assert_eq!(0x1, fb.0);
-   assert_eq!(0x1, fb.foo1());
-   assert_eq!(0x0, fb.foo2());
+    fb.set_foo1(0b11);
+    assert_eq!(0x1, fb.0);
+    assert_eq!(0x1, fb.foo1());
+    assert_eq!(0x0, fb.foo2());
 
 }
 
 #[test]
 fn test_multiple_bit() {
-   let mut fb = FooBar(0);
+    let mut fb = FooBar(0);
 
-   fb.set_foo3(0x0F);
-   assert_eq!(0xF, fb.0);
-   assert_eq!(0xF, fb.foo3());
-   assert_eq!(0x0, fb.foo4());
+    fb.set_foo3(0x0F);
+    assert_eq!(0xF, fb.0);
+    assert_eq!(0xF, fb.foo3());
+    assert_eq!(0x0, fb.foo4());
 
-   fb.set_foo4(0x0F);
-   assert_eq!(0xF000000F, fb.0);
-   assert_eq!(0xF, fb.foo3());
-   assert_eq!(0xF, fb.foo4());
+    fb.set_foo4(0x0F);
+    assert_eq!(0xF000000F, fb.0);
+    assert_eq!(0xF, fb.foo3());
+    assert_eq!(0xF, fb.foo4());
 
-   fb.set_foo3(0);
-   assert_eq!(0xF0000000, fb.0);
-   assert_eq!(0x0, fb.foo3());
-   assert_eq!(0xF, fb.foo4());
+    fb.set_foo3(0);
+    assert_eq!(0xF0000000, fb.0);
+    assert_eq!(0x0, fb.foo3());
+    assert_eq!(0xF, fb.foo4());
 
-   fb.set_foo3(0xA);
-   assert_eq!(0xF000000A, fb.0);
-   assert_eq!(0xA, fb.foo3());
-   assert_eq!(0xF, fb.foo4());
+    fb.set_foo3(0xA);
+    assert_eq!(0xF000000A, fb.0);
+    assert_eq!(0xA, fb.foo3());
+    assert_eq!(0xF, fb.foo4());
 }
 
 #[test]
 fn test_getter_setter_only() {
-   let mut fb = FooBar(0);
-   fb.setter_only(0x7);
-   assert_eq!(0x1C, fb.0);
-   assert_eq!(0x6, fb.getter_only());
+    let mut fb = FooBar(0);
+    fb.setter_only(0x7);
+    assert_eq!(0x1C, fb.0);
+    assert_eq!(0x6, fb.getter_only());
 }
 
 #[test]
